5343ae34d571321c4190a261b0479380302cd4bd
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-07 15:49:42 +0530

    created student router

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,9 +1,9 @@
 //assign dependencies to variables.
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const app = express();
 dotenv.config();
 
@@ -35,9 +35,19 @@ mongoose.connect(URL, {
 
 //once connected to the databse this will use to check it
 const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("Mongodb Connection Success!");
+connection.once('open', () => {
+  console.log('Mongodb Connection Success!');
 });
+
+//create coonnection between front end and backend
+const studetRoute = require('./routes/students_route');
+
+/*By using app.use() method with the '/student' route as the first argument,
+ you are telling Express to use this middleware for any route that starts with '/student'.
+  This means that if a client makes a request to /student/create, /student/view, /student/update, or 
+  any other route that starts with '/student', the request will be passed on to the studetRoute middleware
+   to handle.*/
+app.use('/student', studetRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port : ${PORT}`);
