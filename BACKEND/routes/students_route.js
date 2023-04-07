1ff296a7a17243432e200787a85a3afa0bfbcdd5
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-07 18:01:54 +0530

    CRUD done

--- BACKEND/routes/students_route.js
+++ BACKEND/routes/students_route.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const Student = require('../models/student');
 let student = require('../models/student');
 
 //https//localhost:8070/student/add
@@ -19,7 +20,80 @@ router.route('/add').post((req, res) => {
     .then(() => {
       res.json('Student Added Successfully');
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err.message));
+});
+
+//https//localhost:8070/get/student/
+router.route('/get').get((req, res) => {
+  student
+    .find()
+    .then((students) => {
+      res.json(students);
+    })
+    .catch((err) => console.log(err.message));
+});
+
+//https//localhost:8070/student/update/:sid
+router.route('/update/:sid').put(async (req, res) => {
+  let userID = req.params.sid;
+  const { name, age, gender } = res.body;
+
+  const updateStudent = {
+    name,
+    age,
+    gender,
+  };
+
+  const update = await Student.findByIdAndUpdate(userID, updateStudent)
+    .then(() => {
+      res.status(200).send({
+        status: 'User Updated',
+        user: update,
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send({
+        status: 'Server Error with updating data',
+        error: err.message,
+      });
+    });
+});
+
+//https//localhost:8070/student/delete/:sid
+router.route('/delete/:sid').delete(async (req, res) => {
+  let uId = req.params.sid;
+  await Student.findByIdAndDelete(uId)
+    .then(() => {
+      res.status(200).send({
+        status: 'user Deleted',
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send({
+        status: 'Error with deleting user',
+        error: err.message,
+      });
+    });
+});
+
+//https//localhost:8070/student/get/:sid
+router.route('/get/:sid').get(async (req, res) => {
+  const uID = req.params.sid;
+  const user = await Student.findById(uID)
+    .then(() => {
+      res.status(200).send({
+        status: 'User Fetched',
+        user: user,
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send({
+        status: 'Error with fetch user',
+      });
+    });
 });
 
 module.exports = router;
