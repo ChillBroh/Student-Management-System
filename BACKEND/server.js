29e5f811984d2624e704352f943f713ec878b38e
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-07 14:13:19 +0530

    updated

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -40,5 +40,5 @@ connection.once("open", () => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`);
+  console.log(`Server is up and running on port : ${PORT}`);
 });
