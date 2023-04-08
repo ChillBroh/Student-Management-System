3df272aaa438be2bc307bb9a6178296050329e9a
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-08 16:24:52 +0530

    header imported

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import CounterClass from "./Components/1.CounterClass";
-import CounterFunction from "./Components/2.CounterFunction";
+import Header from "./Components/header";
 
 function App() {
   return (
-    <div className="App">
-      <CounterClass />
-      <hr />
-      <CounterFunction />
+    <div>
+      <Header />
     </div>
   );
 }
