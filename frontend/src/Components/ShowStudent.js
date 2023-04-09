d0c50ec494c98a1bb99e03196f8f72b66612b35d
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-09 14:25:13 +0530

    fetching all students done

--- frontend/src/Components/ShowStudent.js
+++ frontend/src/Components/ShowStudent.js
@@ -1,1 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function ShowStudents() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    const getStudents = () => {
+      axios
+        .get("http://localhost:8070/student/get")
+        .then((res) => {
+          setStudents(res.data);
+        })
+        .catch((err) => alert(err.message));
+    };
+    getStudents();
+  });
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>All students</h1>
+      <div className="container">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Age</th>
+              <th scope="col">Gender</th>
+            </tr>
+          </thead>
+          {students.map((item) => (
+            <tbody>
+              <tr>
+                <td>{item._id}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.gender}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
+}
--- frontend/src/Components/header.js
+++ frontend/src/Components/header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -22,13 +23,18 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="/get">
                   Home
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/add">
                   Create Student
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Show a Student
                 </a>
               </li>
             </ul>
