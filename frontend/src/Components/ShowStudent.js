ec15bbc88dd7ebac8b99de4de8fdb40e533d94cd
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-09 15:47:21 +0530

    id catch completed

--- frontend/src/Components/ShowStudent.js
+++ frontend/src/Components/ShowStudent.js
@@ -14,28 +14,47 @@ export default function ShowStudents() {
         .catch((err) => alert(err.message));
     };
     getStudents();
-  });
+  }, []);
+
+  const update = (event) => {
+    const catchID = event.currentTarget.id;
+  };
 
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>All students</h1>
       <div className="container">
-        <table class="table">
+        <table className="table ">
           <thead>
             <tr>
+              <th scope="col"></th>
               <th scope="col">ID</th>
               <th scope="col">Name</th>
               <th scope="col">Age</th>
               <th scope="col">Gender</th>
             </tr>
           </thead>
-          {students.map((item) => (
+          {students.map((item, count = 0) => (
             <tbody>
               <tr>
+                <td style={{ color: "red" }}>{count + 1}</td>
                 <td>{item._id}</td>
                 <td>{item.name}</td>
                 <td>{item.age}</td>
                 <td>{item.gender}</td>
+                <td>
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    id={item._id}
+                    onClick={update}
+                  >
+                    Update
+                  </button>{" "}
+                  <button type="button" class="btn btn-danger">
+                    Delete
+                  </button>
+                </td>
               </tr>
             </tbody>
           ))}
